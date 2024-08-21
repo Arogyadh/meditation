345419a8de92ae98204c90a1d870a65b164774a3
Arogya Dhaubhadel <arogyadhdl@gmail.com>
2024-08-21 22:27:58 +0545

    add gradients,router and button

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,42 @@
-import { Image, StyleSheet, Platform, Text, View } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
 
-export default function HomeScreen() {
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import CustomButton from "@/components/customButton";
+import { useRouter } from "expo-router";
+import AppGradient from "@/components/AppGradient";
+
+const App = () => {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <View className="mt-6 flex-1">
+            <Text className="text-center text-white bold text-4xl">
+              Meditation
+            </Text>
+            <Text className="text-center text-white text-regular text-2xl mt-3 ">
+              Simplifying Meditation For Everybody
+            </Text>
+          </View>
+          <View>
+            <CustomButton
+              onPress={() => router.push("/nature-meditate")}
+              title="Get Started"
+            />
+          </View>
+        </AppGradient>
+      </ImageBackground>
     </View>
   );
-}
+};
+
+export default App;
